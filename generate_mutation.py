8a9912671235a703a3d6e0851856d68937248029
LuoshuTheBest <lzhangbk@connect.ust.hk>
2020-10-19 14:22:36 +0800

    Made some corresponding changes

---
+++ generate_mutation.py
@@ -0,0 +1,13 @@
+# The executable file of the binding affinity tool based on the Rosetta platform.
+# The estimation does not yield high level of accuracy. The API is designed for rough calculation only.
+from utility.mutation import mutate_atoms
+from utility.peptide_loader import load_peptides
+
+
+if __name__ == '__main__':
+    # load a list of peptide sequence from input file
+    peptides = load_peptides("lib/sample_peptide_sequences.txt")
+    # generate mutated molecule file for further processing
+    print("Starting the mutation of atoms...")
+    for p in peptides:
+        mutate_atoms(str(p))
--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 # The executable file of the binding affinity tool based on the Rosetta platform.
 # The estimation does not yield high level of accuracy. The API is designed for rough calculation only.
-from utility.mutation import mutate_atoms
-from utility.residue_matching_loader import load_residue_matching
-from utility.peptide_loader import load_peptides
 from utility.dock import dock
+import os
 
 
 if __name__ == '__main__':
-    # load a list of peptide sequence from input file
-    peptides = load_peptides("lib/sample_peptide_sequences.txt")
-    # generate mutated molecule file for further processing
-    print("Starting the mutation of atoms...")
-    for p in peptides:
-        mutate_atoms(str(p))
+    os.system("python3 generate_mutation.py")
     print("The mutation of atoms finished. Starting the docking process...")
-    # dock the known peptide sequence and protein
-    print("Docking started.")
-    print(dock())
+    dock()
