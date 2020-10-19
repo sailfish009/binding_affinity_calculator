509a7e9dac7f34e9c0766d597be29fd4b7a74c21
LuoshuTheBest <lzhangbk@connect.ust.hk>
2020-10-19 14:59:08 +0800

    Made some corresponding changes

--- utility/dock.py
+++ utility/dock.py
@@ -1,10 +1,15 @@
 import os
+import sys
 import subprocess
 
 
-def modify_flag():
-    pass
-
-
 def dock():
-    os.system("lib/rosetta/source/bin/docking_protocol.linuxgccrelease @flag_local_refine -overwrite")
+    result = subprocess.run(["lib/rosetta/source/bin/docking_protocol.linuxgccrelease",
+                             "@flag_local_refine", "-overwrite"], stdout=subprocess.PIPE)
+    record = result.stdout
+    lines = record.splitlines()
+    for i in range(len(lines), -1, -1):
+        pass
+    # os.system("lib/rosetta/source/bin/docking_protocol.linuxgccrelease @flag_local_refine -overwrite")
+    return lines
+
