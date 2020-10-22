b23168e04e0bb2b44c64e26829f93f6f589812ab
Luoshu Zhang <lzhangbk@ust.hk>
2020-10-22 10:08:12 +0800

    Updated the result processing

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from utility.dock import dock
 import os
 from utility.side_chain_removal import remove_side_chain
 from utility.dock_peptide import flex_peptide_docking
+from utility.select_docking_result import interpret_docking_result
 
 
 if __name__ == '__main__':
-    '''
     # Execute the mutation source file to change the peptide sequence to the peptide sequence to be calculated
     print("Starting the mutation of atoms...")
     os.system("python3 generate_mutation.py")
@@ -19,5 +19,7 @@ if __name__ == '__main__':
     # Remove the side chain of the MHC to facilitate the further calculation
     print("The docking process completed. Starting the calculation of binding affinity...")
     remove_side_chain()
-    '''
+
+    # Perform the FlexPepDocking and compute the corresponding binding affinity
     flex_peptide_docking()
+    interpret_docking_result("structures/pep_docked_output/score_flexpep_generation.fasc")
