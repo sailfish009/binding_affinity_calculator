7af9ce221519e373aa823dadc2005de7a377670d
LuoshuTheBest <lzhangbk@connect.ust.hk>
2020-10-22 17:35:32 +0800

    Added some results in flexpepdocking

--- utility/dock_peptide.py
+++ utility/dock_peptide.py
@@ -3,4 +3,4 @@ import os
 
 def flex_peptide_docking():
     os.system("lib/rosetta/source/bin/FlexPepDocking.linuxgccrelease @flags_prepack_flex -overwrite")
-    # os.system("lib/rosetta/source/bin/FlexPepDocking.linuxgccrelease @flags_generation_flex")
+    os.system("lib/rosetta/source/bin/FlexPepDocking.linuxgccrelease @flags_generation_flex")
